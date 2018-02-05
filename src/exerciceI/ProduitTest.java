f9f522728ddfccfce1983d2a4cd6493fe133f57c
Bérenger Thévenet <berenger.thevenet@laposte.net>
2018-02-05 20:46:45 +0100

    à

--- src/exerciceI/ProduitTest.java
+++ src/exerciceI/ProduitTest.java
@@ -5,13 +5,13 @@ public class ProduitTest {
 	public static void main(String[] args) {
 		
 		
-		//Création des produits
+		//Crï¿½ation des produits
 		Produit p = new Produit("produit100");
 		Produit p2 = new Produit("produit200");
 		
 		
 		//modification des produits
-		p.setNom("Crayon à papier");
+		p.setNom("Crayon a papier");
 		p.setPrix(1);
 		
 		p2.setNom("Crayon de couleur");
