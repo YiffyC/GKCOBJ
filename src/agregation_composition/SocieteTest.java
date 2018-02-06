00addc4a4ace31f59e2cd23ba5cda294e3ed410a
Bérenger Thévenet <YiffyC@users.noreply.github.com>
2018-02-06 14:28:36 +0100

    Ajout setNom dans societe

--- src/agregation_composition/SocieteTest.java
+++ src/agregation_composition/SocieteTest.java
@@ -15,6 +15,24 @@ public class SocieteTest {
 		//Afficher la raison sociale du service de la division
 		System.out.println(s.getDivision().getService().getRaisonSociale());
 		
+		s.setNom("Orange");
+		s.getDivision().setNom("R&D");
+		s.getDivision().getService().setRaisonSociale("Bureau de recherche en Java");
+		
+		
+		System.out.println("______________________________");
+		
+		
+		//afficher nom de la societe
+		System.out.println("Société : " + s.getNom());
+				
+		//Afficher nom de la division
+		System.out.println("Division : " + s.getDivision().getNom());
+				
+		//Afficher la raison sociale du service de la division
+		System.out.println("Raison sociale : " + s.getDivision().getService().getRaisonSociale());
+		
+		
 
 	}
 
